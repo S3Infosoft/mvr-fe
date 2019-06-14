c471ed9adbc84404550c307c5d80b3558848dcf7
Abhishek Tiwari <abhie.lp@gmail.com>
2019-06-14 22:58:56 +0530

    Converted to ModelSerializer

--- app/enquiry/api/serializers.py
+++ app/enquiry/api/serializers.py
@@ -3,21 +3,21 @@ from .. import models
 from rest_framework import serializers
 
 
-class OTASerializer(serializers.HyperlinkedModelSerializer):
+class OTASerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.OTA
         fields = "__all__"
 
 
-class PartnerSerializer(serializers.HyperlinkedModelSerializer):
+class PartnerSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.Partner
         fields = "__all__"
 
 
-class ReviewSerializer(serializers.HyperlinkedModelSerializer):
+class ReviewSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.Review
