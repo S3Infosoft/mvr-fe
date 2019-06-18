7e740bfc41fc1293f7d8c7bc46b8633892151454
Abhishek Tiwari <abhie.lp@gmail.com>
2019-06-18 11:57:41 +0530

    Added Schedule model

--- app/schedules/models.py
+++ app/schedules/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from enquiry.models import OTA
 
-# Create your models here.
+from django.db import models
+from django.utils.timezone import now
+
+
+class Schedule(models.Model):
+    property_name = models.CharField(max_length=150)
+    ota_name = models.ForeignKey(OTA, on_delete=models.CASCADE)
+    check_in_date = models.DateTimeField(default=now)
+    check_out_date = models.DateTimeField(default=now)
+    listing_position_number = models.PositiveIntegerField()
+    room_category_and_rates = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.property_name + " " + self.ota_name.name
