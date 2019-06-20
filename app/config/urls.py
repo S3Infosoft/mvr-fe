69076396edda7d9336cfcd3824be069650478784
Abhishek Tiwari <abhie.lp@gmail.com>
2019-06-20 12:22:16 +0530

    Added media path

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("schedule/", include("schedules.urls", namespace="schedule")),
@@ -7,5 +9,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/v1/", include("enquiry.api.urls")),
     path("", include("users.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
