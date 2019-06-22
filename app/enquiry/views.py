bf8b6f9132e783f3fff8f63caeeb7dfaaa3f9fce
Abhishek Tiwari <abhie.lp@gmail.com>
2019-06-22 08:40:14 +0530

    Added form as a context

--- app/enquiry/views.py
+++ app/enquiry/views.py
@@ -1,3 +1,5 @@
+from . import forms
+
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
@@ -6,18 +8,22 @@ from django.contrib.auth.decorators import login_required
 def ota_list(request):
     if request.is_ajax():
         return render(request, "enquiry/jquery_snippets/ota_list_jquery.html")
-    return render(request, "enquiry/ota_list.html")
+    return render(request, "enquiry/ota_list.html", {"form": forms.OTAForm()})
 
 
 @login_required
 def partner_list(request):
     if request.is_ajax():
-        return render(request, "enquiry/jquery_snippets/partner_list_jquery.html")
-    return render(request, "enquiry/partner_list.html")
+        return render(request,
+                      "enquiry/jquery_snippets/partner_list_jquery.html")
+    return render(request, "enquiry/partner_list.html",
+                  {"form": forms.PartnerForm()})
 
 
 @login_required
 def review_list(request):
     if request.is_ajax():
-        return render(request, "enquiry/jquery_snippets/review_list_jquery.html")
-    return render(request, "enquiry/review_list.html")
+        return render(request,
+                      "enquiry/jquery_snippets/review_list_jquery.html")
+    return render(request, "enquiry/review_list.html",
+                  {"form": forms.ReviewForm()})
