f0225d857832b84a33a4c7d7f58cd1dc8f1afdf9
Abhishek Tiwari <abhie.lp@gmail.com>
2019-06-25 13:13:16 +0530

    Added Ativity serializer

--- app/enquiry/api/serializers.py
+++ app/enquiry/api/serializers.py
@@ -1,6 +1,22 @@
 from .. import models
 
+from easyaudit.models import CRUDEvent
 from rest_framework import serializers
+
+
+class CRUDEventSerializer(serializers.ModelSerializer):
+    event_type = serializers.SerializerMethodField()
+    content_type = serializers.StringRelatedField()
+    user = serializers.StringRelatedField()
+
+    class Meta:
+        model = CRUDEvent
+        exclude = ("object_id", "user_pk_as_string",
+                   "object_json_repr")
+
+    @staticmethod
+    def get_event_type(obj):
+        return obj.get_event_type_display()
 
 
 class OTASerializer(serializers.ModelSerializer):
