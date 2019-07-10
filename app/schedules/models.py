1f11b94727aa56a956adbe3ac1912043d7f7ac25
Abhishek Tiwari <abhie.lp@gmail.com>
2019-07-11 03:26:58 +0530

    Added db_index, null and blank to some fields

--- app/schedules/models.py
+++ app/schedules/models.py
@@ -2,7 +2,6 @@ from enquiry.models import OTA
 
 from django.conf import settings
 from django.db import models
-from django.utils.timezone import now
 
 
 class Schedule(models.Model):
@@ -15,16 +14,25 @@ class Schedule(models.Model):
 
     creator = models.ForeignKey(settings.AUTH_USER_MODEL,
                                 on_delete=models.CASCADE)
-    property_name = models.CharField(max_length=150)
+    property_name = models.CharField(max_length=150, blank=True,
+                                     null=True)
     ota_name = models.ForeignKey(OTA, on_delete=models.CASCADE)
-    check_in_date = models.DateTimeField(default=now)
-    check_out_date = models.DateTimeField(default=now)
-    listing_position_number = models.PositiveIntegerField()
-    room_category_and_rates = models.TextField(max_length=200)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(choices=STATUS, max_length=10)
+    check_in_date = models.DateTimeField(blank=True, null=True)
+    check_out_date = models.DateTimeField(blank=True, null=True)
+    listing_position_number = models.PositiveIntegerField(blank=True,
+                                                          null=True)
+    room_category_and_rates = models.TextField(max_length=200,
+                                               blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True, blank=True, null=True,
+                                   db_index=True)
+    status = models.CharField(choices=STATUS, max_length=10,
+                              blank=True, null=True,
+                              default="PENDING", db_index=True)
     comments = models.TextField(blank=True, null=True)
 
+    class Meta:
+        ordering = "-id",
+
     def __str__(self):
-        return self.property_name + " " + self.ota_name.name
+        return self.ota_name.name
