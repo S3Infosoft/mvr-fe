bb4c530203777ef47e172d0f59e52db7a0f6b776
harshita269 <harshitakhare2626@gmail.com>
2020-04-24 01:08:38 +0530

    removed import

--- app/config/urls.py
+++ app/config/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-import debug_toolbar
 
 urlpatterns = [
     path("schedule/", include("schedules.urls", namespace="schedule")),
